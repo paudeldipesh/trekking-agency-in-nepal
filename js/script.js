d748651a9e7282cab6632fb1fedf4e2ff22a2016
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2024-02-01 16:05:26 +0545

    mobile nav completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
-// const navItems = document.querySelectorAll("#nav-item");
+const hamburgerIcon = document.getElementById("bar-icon");
+const mobileNavbar = document.getElementById("nav-items");
 
-// navItems.forEach((navItem) => {
-//   navItem.addEventListener("mouseenter", () => {
-//     navItem
-//   });
+hamburgerIcon.addEventListener("click", () => {
+  if (mobileNavbar.style.display !== "block") {
+    mobileNavbar.style.display = "block";
+  } else {
+    mobileNavbar.style.display = "none";
+  }
+});
 
-//   navItem.addEventListener("mouseleave", () => {
-//     subNavItem.classList.add("hidden");
-//   });
-// });
+document.querySelector(".trekking").addEventListener("click", () => {
+  document.querySelector(".sub-trekking").classList.toggle("hidden");
+});
+
+document.querySelector(".manaslu").addEventListener("click", () => {
+  document.querySelector(".sub-manaslu").classList.toggle("hidden");
+});
+
+document.querySelector(".everest").addEventListener("click", () => {
+  document.querySelector(".sub-everest").classList.toggle("hidden");
+});
+
+document.querySelector(".annapurna").addEventListener("click", () => {
+  document.querySelector(".sub-annapurna").classList.toggle("hidden");
+});
+
+document.querySelector(".langtang").addEventListener("click", () => {
+  document.querySelector(".sub-langtang").classList.toggle("hidden");
+});
+
+document.querySelector(".dolpa").addEventListener("click", () => {
+  document.querySelector(".sub-dolpa").classList.toggle("hidden");
+});
+
+document.querySelector(".climbing").addEventListener("click", () => {
+  document.querySelector(".sub-climbing").classList.toggle("hidden");
+});
+
+document.querySelector(".tour").addEventListener("click", () => {
+  document.querySelector(".sub-tour").classList.toggle("hidden");
+});
+
+document.querySelector(".helicopter").addEventListener("click", () => {
+  document.querySelector(".sub-helicopter").classList.toggle("hidden");
+});
