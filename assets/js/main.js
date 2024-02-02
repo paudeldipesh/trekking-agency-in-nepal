02f4eb3e7c2740ce2512945976d2019e95128cbe
PrithakADHI <prithakad61@gmail.com>
2024-02-02 14:19:26 +0545

    progress on show

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// function toggleFaq(faqId) {
+//     var answer = document.getElementById(faqId);
+//     answer.classList.toggle("show");
+
+//     var lastChar = faqId[faqId.length - 1];
+//     console.log("arrow" + lastChar);
+//     var accordianElement = document.getElementById("arrow" + lastChar);
+    
+//     if (accordianElement.innerHTML === '<i class="fa fa-angle-down"></i>') {
+//         accordianElement.innerHTML = '<i class="fa fa-angle-up"></i>';
+//     } else {
+//         accordianElement.innerHTML = '<i class="fa fa-angle-down"></i>';
+//     }
+
+//   }
+
+
+function toggleFaq(faqId) {
+
+    var no = faqId[faqId.length - 1];
+    if (faqId[0] == 'i') {
+    var faqElement = document.querySelector('.faq[data-accordionid="' + no + '"]');
+    var answer = faqElement.querySelector('.answer');
+    } else {
+        var faqElement = document.querySelector('.faq[data-faqid="' + no + '"]');
+        var answer = faqElement.querySelector('.answer');        
+    }
+    answer.classList.toggle("show");
+
+    var arrowElement = faqElement.querySelector('.arrow');
+    arrowElement.classList.toggle("up");
+
+    console.log("arrow" + (arrowElement.classList.contains("up") ? " up" : " down"));
+}
+
+
+var Swipes = new Swiper('.swiper-container', {
+    loop: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
